0280446ecb086d397c14632be1deb631d1be9295
Eduar <edcorgriz@hotmail.com>
2022-09-14 20:40:36 -0500

    Add files via upload

---
+++ EduarC/Python/nuevo.py
@@ -0,0 +1,10 @@
+"""nombre = "Eduar"
+edad = 41
+print("Hola",nombre,"tienes",edad,"años")"""
+"""nombre = input("Ingrese su nombre:")
+edad = input ("Ingrese su edad:")
+print("Hola",nombre,"tienes",edad,"años")"""
+num1= int(input("Ingrese un numero:"))
+num2 =int(input ("Ingrese el segundo numero:"))
+sum =num1+num2
+print("la suma",num1,"+",num2,"=",sum)
