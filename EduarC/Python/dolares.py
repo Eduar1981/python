91a0b7340e0d75460a59ccb20ab32c42d559c4e8
Eduar1981 <edcorgriz@hotmail.com>
2022-09-16 22:14:19 -0500

    segundo

---
+++ EduarC/Python/dolares.py
@@ -0,0 +1,3 @@
+num1=float(input("Ingrese la tasa de cambio del dolar hoy, con respecto al peso colombiano:"))
+num2=float(input("Ingrese la cantidad de pesos colombinos:"))
+print("La cantidad de dolares es:",(num2/num1))
