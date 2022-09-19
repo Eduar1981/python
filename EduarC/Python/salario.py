c243c6b67aa603b536ac2cd642ac6dd412c2988d
Eduar1981 <edcorgriz@hotmail.com>
2022-09-19 16:55:10 -0500

    fifth

---
+++ EduarC/Python/salario.py
@@ -0,0 +1,5 @@
+"""Calcular el nuevo salario de un obrero si obtuvo un incremento del 25% sobre su salario anterior."""
+name=(input("Ingrese el nombre del empleado:"))
+salary=float(input("Ingrese el valor del ultimo salalrio:"))
+salary=salary+(salary*0.25)
+print("El nuevo salario de Eduar es:",salary)
