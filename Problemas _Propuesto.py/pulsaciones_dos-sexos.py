a112debfe725e935b51bd8ccd644c53b25453fab
Eduar1981 <edcorgriz@hotmail.com>
2022-10-06 21:45:26 -0500

    problemas

--- Problemas _Propuesto.py/pulsaciones_dos-sexos.py
+++ Problemas _Propuesto.py/pulsaciones_dos-sexos.py
@@ -5,7 +5,7 @@ y si el sexo es masculino:
 num. pulsaciones = (210 - edad)/10"""
 
 edad=int(input("Ingrese su edad:"))
-numero=int(input("Ingrese un numero deacuerdo a tu genero, 1 es igual al sexo femenino y 2 es igual al sexo masculino :"))
+numero=int(input("Ingrese un numero de acuerdo a tu genero, 1 es para  sexo femenino y 2 es para sexo masculino :"))
 pulsaciones=float
 
 if numero == 1:
@@ -16,4 +16,4 @@ elif numero == 2 :
     pulsaciones=(210-edad)/10
 else:
     input("Escribe un numero correcto:")
-print("El numero de tus pusaciones deacuerdo a tu edad es: ",pulsaciones)
+print("El numero de tus pusaciones de acuerdo a tu edad es: ",pulsaciones)
